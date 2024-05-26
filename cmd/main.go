bd6c307968c8b8933de8a57c2b7c7908bfe521c0
Andres Reyna <andresreyna15@gmail.com>
2024-05-26 12:56:38 -0600

    RemoveEdge added

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"ds/ds"
+	"fmt"
 )
 
 func main() {
-	graph := ds.DirectedGraph[string]{}
+	graph := ds.Graph[string]{}
 	graph.AddVertex("Mante")
 	graph.AddVertex("Victoria")
 	graph.AddVertex("Tula")
@@ -16,20 +17,34 @@ func main() {
 	graph.AddEdge(0, 1)
 	graph.AddEdge(0, 2)
 	
-	graph.AddEdge(1, 0)
 	graph.AddEdge(1, 3)
 	graph.AddEdge(1, 4)
 	graph.AddEdge(1, 5)
 	
 	graph.AddEdge(2, 1)
 	
-	graph.AddEdge(3, 3)
 	graph.AddEdge(3, 4)
 	
 	graph.AddEdge(4, 5)
 	
 	graph.AddEdge(5, 3)
-	graph.AddEdge(5, 4)
 
+	graph.PrintGraph()
+	
+	
+	n1, n2 := 4, 3
+	isNeighbor := graph.IsNeighbor(n1, n2)	
+	fmt.Printf("%v IsNeighbor of %v = %v\n", graph.GetValue(n1), graph.GetValue(n2), isNeighbor)
+	
+	n1, n2 = 0, 4
+	isNeighbor = graph.IsNeighbor(n1, n2)	
+	fmt.Printf("%v IsNeighbor of %v = %v\n", graph.GetValue(n1), graph.GetValue(n2), isNeighbor)
+	
+	graph.RemoveEdge(1,0)
+	graph.RemoveEdge(2,1)
+	graph.RemoveEdge(4,5)
+	graph.PrintGraph()
+
+	graph.RemoveVertex("Mante")	
 	graph.PrintGraph()
 }
--- ds/graph.go
+++ ds/graph.go
@@ -7,19 +7,19 @@ type Vertex[T comparable] struct {
 	val T
 }
 
-type DirectedGraph[T comparable] struct {
+type Graph[T comparable] struct {
 	edges map[int][]int
 	vertices []Vertex[T]
 }
 
-func (g DirectedGraph[T]) Size() int {
+func (g Graph[T]) Size() int {
 	if g.vertices != nil {
 		return len(g.vertices)
 	}
 	return 0
 }
 
-func (g DirectedGraph[T]) GetValue(idx int) T {
+func (g Graph[T]) GetValue(idx int) T {
 	var value T
 	if g.Size() >= 1 {
 		value = g.vertices[idx].val
@@ -28,7 +28,7 @@ func (g DirectedGraph[T]) GetValue(idx int) T {
 }
 
 // Print the graph with values
-func (g DirectedGraph[T]) PrintGraph() {
+func (g Graph[T]) PrintGraph() {
 	fmt.Printf("Graph: \n")
 	
 	for v, edges := range g.edges {
@@ -41,7 +41,7 @@ func (g DirectedGraph[T]) PrintGraph() {
 }
 
 // Insert a new vertex into the graph
-func (g *DirectedGraph[T]) AddVertex(value T) int {
+func (g *Graph[T]) AddVertex(value T) int {
 	if g.Size() == 0 {
 		g.vertices = []Vertex[T]{}
 		g.edges = map[int][]int{}
@@ -52,7 +52,54 @@ func (g *DirectedGraph[T]) AddVertex(value T) int {
 	return newNode.id
 }
 
+
 // Insert a new edge into the graph
-func (g *DirectedGraph[T]) AddEdge(x, y int) {
-	g.edges[x] = append(g.edges[x], y)	
+func (g *Graph[T]) AddEdge(x, y int) {
+	g.edges[x] = append(g.edges[x], y)
+	g.edges[y] = append(g.edges[y], x)
+}
+
+// Checks if x is neighbor of y
+func (g *Graph[T]) IsNeighbor(x, y int) int {
+	for idx, v := range g.edges[y] {
+		if v == x {
+			return idx
+		}
+	}
+	return -1
+}
+
+// Remove edge between x and y
+func (g *Graph[T]) RemoveEdge(x, y int) {
+	neighborIdx := g.IsNeighbor(x, y)
+	if neighborIdx != -1 {
+		g.edges[y] = append(g.edges[y][:neighborIdx], g.edges[y][neighborIdx+1:]...)
+	}
+}
+
+// Remove vertex from the graph
+func (g *Graph[T]) RemoveVertex(value T) int {
+	if g.Size() == 0 {
+		return -1
+	}
+	
+	var vertexId int
+		
+	for v := range g.vertices {
+		if g.vertices[v].val == value {
+			vertexId = g.vertices[v].id
+			g.vertices = append(g.vertices[:vertexId], g.vertices[vertexId+1:]...)
+		}
+	}
+	
+	// edges map[int][]int
+	delete(g.edges, vertexId)
+	for vertex, edges := range g.edges {
+		neighborIdx := g.IsNeighbor(vertexId, vertex)
+		if neighborIdx != -1 {
+			g.edges[vertex] = append(edges[:vertex], edges[vertex+1:]...)
+		}
+	}
+
+	return vertexId
 }
